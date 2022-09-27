1ff7fd5858001c8ce9cc0742633344b91676dddf
flpdorea <felipeldorea@gmail.com>
2022-09-27 14:40:53 -0300

    passing all Controller tests

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,14 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id }: any = request.params;
+
+    try {
+      const adminUser = this.turnUserAdminUseCase.execute({ user_id });
+      return response.json(adminUser).send();
+    } catch (error) {
+      return response.status(404).json({ error: error.message }).send();
+    }
   }
 }
 
