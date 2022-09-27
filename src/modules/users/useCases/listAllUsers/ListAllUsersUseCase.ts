17d71e37728b0e5bbe22392d0b482c64396024af
flpdorea <felipeldorea@gmail.com>
2022-09-27 12:25:39 -0300

    passing all UseCase tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const isAdmin = this.usersRepository.findById(user_id);
+
+    if (!isAdmin.admin) {
+      throw new Error("Unauthorized!");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
